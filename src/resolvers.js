94bc9e19f21b81c7cdd0ea67e5904e1707656d51
ederpbj <ederpbj@yahoo.com.br>
2020-01-09 12:55:39 -0300

    finalizando projeto

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,18 +1,23 @@
-const users = [
-    { id: 1, name: 'Eder', email: 'ederpbj@gmail.com' },
-    { id: 2, name: 'Eduardo', email: 'edu@gmail.com' },
-]
+//importa schema mongodb
+const User = require('./User')
+
+/**Testando usuário 
+ const users = [
+     { id: 1, name: 'Eder', email: 'ederpbj@gmail.com' },
+     { id: 2, name: 'Eduardo', email: 'edu@gmail.com' },
+    ]
+ */
 
 //Baseado no schema.graphql
 //Faz as consultas
 module.exports = {
     Query: {
-        users: () => users,
-        user: () => user[0]
+        users: (_, { page, per_page, filters }) => User.find(),
+        user: (_, {id}) => User.findById(id),
     },
 
     Mutation: {
         //createUser: () => {}
-        createUser: () => user[0]
+        createUser: (_, {name, email}) => User.create({name, email}),
     }
 }
