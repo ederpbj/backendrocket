dab97ac19e888de1a2ff915bb57bab39889f5821
ederpbj <ederpbj@yahoo.com.br>
2020-01-09 12:36:17 -0300

    conectar ao mongo

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 //Servidor
 const { GraphQLServer } = require ('graphql-yoga')
 const path =  require ('path')
+const mongoose = require('mongoose')
 
 const resolvers = require('./resolvers')
 
 //Outra forma
 //import { GraphQLServer } from 'graphql-yoga'
 //import path from 'path'
+
+//conectar ao mongo
+mongoose.connect('mongodb://localhost:27017/graphqlnode',{
+    useNewUrlParser: true
+})
 
 const server = new GraphQLServer({
     //definindo type
